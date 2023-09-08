6a363ee332e39b04c1785fe14171c99b2628cf56
haneul <sky9196@naver.com>
2023-09-08 16:45:47 +0900

    main_end

--- js/index.js
+++ js/index.js
@@ -12,21 +12,34 @@ const main_slide = new Swiper('#main_slide',{
     autoplay:{delay:2500}, // delay 속도
     loop:true,
     direction:'horizontal',
-
+    spaceBetween:10,
+    // disableOnInteraction:false,
     pagination:{
         el: '.swiper-pagination',
         clickable:true
     }
 });
+const s5_slide = new Swiper('#s5_slide',{
+    //자동재생-중가로({}) 묶어줘야함
+    autoplay:{delay:2500}, // delay 속도
+    loop:true,
+    slidesPerView:1,
+    direction:'horizontal',
+    // pagination:{
+    //     el: '.swiper-pagination',
+    //     clickable:true
+    // }
+});
 //--------------------------------------------------------
-const tabTitle = document.querySelectorAll ('.tab_title > a')
-const tabContents = document.querySelectorAll ('.sub')
+const tabTitle = document.querySelectorAll('.tab_title > a')
+const tabContents = document.querySelectorAll('.sub')
 const button = document.querySelectorAll('.butn')
 const s2 = document.querySelector('.s2')
-console.log(tabTitle, tabContents)
+console.log(tabTitle, tabContents,s2)
 //---------------------
 for(let j of button){j.style.display = 'none'}
 for(let i of tabContents){i.style.display = 'none'}
+
 tabTitle.forEach(function(t,i,a){
     t.addEventListener('click',function(e){
         e.preventDefault()
@@ -35,22 +48,27 @@ tabTitle.forEach(function(t,i,a){
         for(let i of tabContents){i.style.display = 'none'}
         tabContents[i].style.display = 'block'
         for(let i of button){i.style.display = 'none'}
-        button[i].style.display = 'block',
-        s2.css('backgorund-image','url(../imges/main2.png)')
+        button[i].style.display = 'block'
+        s2.style.backgroundImage = `url(./imges/main${i}.png)`
     })
 })
-// tabTitle.addEventListener('click',function(e){
-//     $('s1').css('backgorund-image','url(../imges/main2.png)')
+/*----------------숫자 올라가는 효과----------------------- */
+// $(function() {
+//     var cnt0 = 0;
+//     timeCounter();
+//     function timeCounter() {
+//         id0 = setInterval(count0Fn, 2.597649);
+//         function count0Fn() {
+//             cnt0++;
+//             if (cnt0 > 38,496,337) {
+//                 clearInterval(id0);
+//             } 
+//             else {
+//                 $(".number").eq(0).text(cnt0);
+//             }
+//         }
+//     }
 // })
-const h_grap = document.querySelector('.contents .h_grap')
-const f_grap = document.querySelector('.contents .f_grap')
-const title = document.querySelector('.s4 .title')
-console.log(h_grap,f_grap,title)
-gsap.to(f_grap,{
-    scrollTriger:title,
-    scaleX:1,
-    duration:3,
-    delay:3,
-    ease:"power2.out"
-    // duration:2
-})
+// function Fullpage__init(){
+    
+// }
